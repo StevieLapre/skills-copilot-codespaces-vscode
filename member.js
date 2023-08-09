c09417af4b0c615057ed3c4a7781bbed4f9cacdc
StevieLapre <139445870+StevieLapre@users.noreply.github.com>
2023-08-09 22:42:41 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,12 +1,15 @@
-function skillMembers() {
-  return {
-    restrict: 'E',
-    templateUrl: 'app/components/skill-members/skill-members.html',
-    controller: 'SkillMembersController',
-    controllerAs: 'vm',
-    bindToController: true,
-    scope: {
-      skill: '='
+function skillsmembers() {
+    var skills = document.getElementById("skills").value;
+    var members = document.getElementById("members").value;
+    var skills = skills.split(",");
+    var members = members.split(",");
+    var result = [];
+    for (var i = 0; i < skills.length; i++) {
+        for (var j = 0; j < members.length; j++) {
+            if (skills[i] == members[j]) {
+                result.push(skills[i]);
+            }
+        }
     }
-  };
+    document.getElementById("result").innerHTML = result;
 }
